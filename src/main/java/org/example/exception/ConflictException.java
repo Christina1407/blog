8ee0bdd4826578ea915052509fca905cf1391762
Christina <christina.lorents@yandex.ru>
2025-02-06 23:38:47 +0300

    added exceptions

--- src/main/java/org/example/exception/ConflictException.java
+++ src/main/java/org/example/exception/ConflictException.java
@@ -10,4 +10,5 @@ public class ConflictException extends RuntimeException {
         super(message);
         this.reason = reason;
     }
+
 }
---
+++ src/main/java/org/example/exception/ExceptionsHandler.java
@@ -0,0 +1,38 @@
+package org.example.exception;
+
+import org.example.controller.PostController;
+import org.example.controller.UserController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice(assignableTypes = {UserController.class, PostController.class})
+public class ExceptionsHandler {
+
+  @ExceptionHandler(NotFoundException.class)
+    public ExceptionResponse handleNotFoundException(NotFoundException ex) {
+        return new ExceptionResponse(HttpStatus.NOT_FOUND.name(), "The required object was not found", ex.getMessage(),
+                LocalDateTime.now());
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ExceptionResponse handleConflictException(final ConflictException ex) {
+        return new ExceptionResponse(HttpStatus.CONFLICT.name(), ex.getReason(), ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ExceptionResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        return new ExceptionResponse(HttpStatus.BAD_REQUEST.name(), "Incorrectly made request",
+                errorMessage, LocalDateTime.now());
+    }
+}
