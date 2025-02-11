edb6aeba89517b354fdd2b526b07879a5dcba127
Christina <christina.lorents@yandex.ru>
2025-02-11 23:24:34 +0300

    updated configuration for frontend

--- src/main/java/org/example/configuration/WebConfiguration.java
+++ src/main/java/org/example/configuration/WebConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.validation.beanvalidation.MethodValidationPostProcess
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -31,6 +32,12 @@ public class WebConfiguration implements WebMvcConfigurer {
 
         argumentResolvers.add(pageableResolver);
         argumentResolvers.add(sortResolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("/WEB-INF/static/");
     }
 
     @Bean
